57dd9250ff1434042ac2e4942a34b5ddd8fdbf3c
Folorunsho-Roberts David <your-email@example.com>
2025-05-20 13:33:22 +0100

    Appbar containing the spotify logo on the signup page has been implemented

--- lib/common/widgets/button/appbar/app_bar.dart
+++ lib/common/widgets/button/appbar/app_bar.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/common/widgets/button/helpers/is_darkmode.dart';
+import 'package:flutter_application_1/core/configs/theme/assets/app_vectors.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class BasicAppbar extends StatelessWidget {
-  const BasicAppbar({super.key});
+class BasicAppbar extends StatelessWidget implements PreferredSizeWidget {
+  final Widget? title;
+  const BasicAppbar({this.title, super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +14,22 @@ class BasicAppbar extends StatelessWidget {
           Colors
               .transparent, // color of the background surrounding the back button
       elevation: 0,
+      title: SvgPicture.asset(AppVectors.logo),
       leading: IconButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.pop(context); // the action for the back button
+        },
         icon: Container(
           height: 50, // height of the environment containing the back buttton
           width: 50, // width of the environment containing the back buttton
           decoration: BoxDecoration(
             // ignore: deprecated_member_use
-            color: Colors.white.withOpacity(0.03),
+            color:
+                context.isDarkMode
+                    // ignore: deprecated_member_use
+                    ? Colors.white.withOpacity(0.03)
+                    // ignore: deprecated_member_use
+                    : Colors.black.withOpacity(0.04),
             shape:
                 BoxShape
                     .circle, // environment that will contain the back button
@@ -27,14 +38,13 @@ class BasicAppbar extends StatelessWidget {
           child: Icon(
             Icons.arrow_back_ios_new,
             size: 15,
-            color:
-                context.isDarkMode
-                    ? Colors.white
-                    : Colors
-                        .black, // color implementation for the back button on switching of the app theme
+            color: context.isDarkMode ? Colors.white : Colors.black, // c
           ), // the back button using the ios type back button
         ),
       ),
     );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight); // has to do with the implemented Preffered size widget above on line 4
 }
--- lib/presentation/auth/pages/signup.dart
+++ lib/presentation/auth/pages/signup.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/common/widgets/button/appbar/app_bar.dart';
 
 class Signup extends StatelessWidget {
   const Signup({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Column(children: []));
+    return const Scaffold(
+      appBar: BasicAppbar(), // the empty page that all items will be built upon
+      body: Column(children: []),
+    );
   }
 }
